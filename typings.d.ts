9ea3b032ed78481269338ccc25dedbba00e9c715
László Károlyi <laszlo@karolyi.hu>
2024-07-30 19:36:02 +0200

    Webpack 4 & 5 compatibility

--- typings.d.ts
+++ typings.d.ts
@@ -1,99 +1,129 @@
-import { Compiler } from 'webpack';
-
-export = BundleTrackerPlugin;
+import { Compiler } from 'webpack'
+export = BundleTrackerPlugin
 
 declare class BundleTrackerPlugin {
-  constructor(options?: BundleTrackerPlugin.Options);
-  apply(compiler: Compiler): void;
-}
+  constructor(options?: BundleTrackerPlugin.Options)
+  apply(compiler: Compiler): void}
+
 declare namespace BundleTrackerPlugin {
   interface Options {
+
     /**
      * Output directory of the bundle tracker JSON file
      * Default: `'.'`
      */
-    path?: string;
+    path?: string
+
     /**
      * Name of the bundle tracker JSON file.
      * Default: `'webpack-stats.json'`
      */
-    filename?: string;
+    filename?: string
+
     /**
      * Property to override default `output.publicPath` from Webpack config file.
      */
-    publicPath?: string;
+    publicPath?: string
+
     /**
      * Output `startTime` and `endTime` properties to JSON file.
      * Default: `false`
      */
-    logTime?: boolean;
+    logTime?: boolean
+
     /**
      * Output relative path from JSON file into `path` properties.
      * Default: `false`
      */
-    relativePath?: boolean;
+    relativePath?: boolean
+
     /**
      * Indent JSON output file
      */
-    indent?: number;
+    indent?: number
+
     /**
      * Enable subresources integrity
      * Default: `false`
      */
-    integrity?: boolean;
+    integrity?: boolean
+
     /**
      * Set subresources integrity hashes
      * Default: `[ 'sha256', 'sha384', 'sha512' ]`
      */
-    integrityHashes?: string[];
+    integrityHashes?: string[]
   }
+
+  interface ComputedOpts {
+    path: string
+    filename: string
+    publicPath: string
+    logTime: boolean
+    relativePath: boolean
+    indent: number
+    integrity: boolean
+    integrityHashes: string[]
+    outputChunkDir: str
+    outputTrackerFile: str
+    outputTrackerDir: str
+  }
+
   interface Contents {
+
     /**
      * Status of webpack
      */
-    status: string;
+    status: string
+
     /**
      * Error when webpack has failure from compilation
      */
-    error?: string;
+    error?: string
+
     /**
      * Error message when webpack has failure from compilation
      */
-    message?: string;
+    message?: string
+
     /**
      * File information
      */
     assets: {
       [name: string]: {
-        name: string;
-        integrity?: string;
-        publicPath?: string;
-        path: string;
-      };
-    };
+        name: string
+        integrity?: string
+        publicPath?: string
+        path: string
+        sourceFilename?: string
+      }    }
+
     /**
      * List of chunks builded
      */
     chunks: {
       [name: string]: [
         {
-          name: string;
-          publicPath?: string;
-          path: string;
+          name: string
+          publicPath?: string
+          path: string
         },
-      ];
-    };
+      ]
+    }
+
     /**
      * Public path of chunks
      */
-    publicPath?: string;
+    publicPath?: string
+
     /**
      * Start time of webpack compilation
      */
-    startTime?: number;
+    startTime?: number
+
     /**
      * End time of webpack compilation
      */
-    endTime?: number;
+    endTime?: number
   }
 }
